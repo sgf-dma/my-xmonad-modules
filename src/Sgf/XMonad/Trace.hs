e8429dc07027e5fec6fb7559368467dd2284c121
sgf <sgf.dma@gmail.com>
2015-09-30 15:45:51 +0300

    trace. Add logHook trace for all windows and manageHook trace for new windows.
    Though, because ManageHook monadic actions evaluated before new window is
    added to windowset in XState, i can't show new window workspace from
    ManageHook.

--- src/Sgf/XMonad/Trace.hs
+++ src/Sgf/XMonad/Trace.hs
@@ -1,11 +1,14 @@
 
 module Sgf.XMonad.Trace
-    ( traceWindowSet
+    ( traceCurWorkspace
+    , traceAllWindows
     , traceFloat
+    , traceNew
     )
   where
 
 import Data.Maybe
+import Data.Monoid
 import qualified Data.Map as M
 import Control.Applicative
 import Control.Monad
@@ -15,15 +18,23 @@ import qualified XMonad.StackSet as W
 
 
 -- Log all windows (tiled and floating).
-traceWindowSet :: X ()
-traceWindowSet      = do
-    trace "Windows:"
+traceCurWorkspace :: X ()
+traceCurWorkspace   = do
+    trace "Windows on current workspace:"
     withWindowSet $ \ws -> do
       whenJust (W.stack . W.workspace . W.current $ ws) $ \s -> do
         ts <- mapM (runQuery title) (W.integrate s)
         trace $ "Tiled: " ++ show ts
       fs <- mapM (runQuery title) (M.keys . W.floating $ ws)
       unless (null fs) $ trace ("Floating: " ++ show fs)
+
+traceAllWindows :: X ()
+traceAllWindows     = do
+    trace "All windows:"
+    withWindowSet $ \ws -> forM_ (W.workspaces ws) $ \w ->
+      whenJust (W.stack w) $ \s -> do
+        ts <- mapM (runQuery title) (W.integrate s)
+        trace $ "On workspace '" ++ W.tag w ++ "': " ++ show ts
 
 -- Log windows, which would be made floating by default (particularly, by
 -- `manage` from XMonad/Operations.hs), and why they would.
@@ -42,4 +53,19 @@ traceFloat          = do
       when (isFixedSize || isTransient) $
         trace (f ++ show w ++ " \"" ++ t ++ "\"")
     return idHook
+
+-- Note, that i can't print new window workspace from ManageHook: user's
+-- ManageHook evaluated in `manage` in XMonad/Operations.hs *before* new
+-- window is added to 'windowset' in XState. Then, `windows` is called with
+-- composition of (WindowSet -> WindowSet) functions: first function adds new
+-- window to WindowSet, second is the one returned by user's ManageHook.
+-- Thus, all monadic actions in ManageHook (like `trace`) run before new
+-- window is added to current WindowSet and have not access to new window
+-- workspace, but user's ManageHook pure function (WindowSet -> WindowSet) run
+-- after and can find workspace of new window.
+traceNew :: ManageHook
+traceNew            = do
+    t <- title
+    trace $ "New window: \"" ++ t ++ "\""
+    return (Endo id)
 
