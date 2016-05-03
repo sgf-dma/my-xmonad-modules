53fb82a6422c66160d451887fb5bc5e88e27dfc6
sgf <sgf.dma@gmail.com>
2016-05-03 21:11:44 +0300

    Save

--- src/Sgf/XMonad/Util/Run.hs
+++ src/Sgf/XMonad/Util/Run.hs
@@ -17,7 +17,7 @@ import Control.Monad.Trans
 
 -- Variants of spawnPipe and spawnPID running process directly (not through
 -- shell).
-spawnPipe' :: FilePath -> [String] -> X (Handle, ProcessID)
+spawnPipe' :: MonadIO m => FilePath -> [String] -> m (Handle, ProcessID)
 spawnPipe' x xs     = io $ do
                         (rd, wr) <- createPipe
                         setFdOption wr CloseOnExec True
@@ -34,4 +34,33 @@ spawnPID' x xs      = xfork $ executeFile x True xs Nothing
 
 spawn' :: MonadIO m => FilePath -> [String] -> m ()
 spawn' x xs         = void (spawnPID' x xs)
+
+-- Guess stack local bin path. Dereference link completely or this does not
+-- matter?
+stackLocalBin :: MonadIO m => m FilePath
+stackLocalBin	    = do
+			readSymbolicLink
+
+
+-- Execute file, which tries stack's local bin path, if binary not found in
+-- PATH.
+executeFile' ...
+
+spawnPID'Stack :: MonadIO m => FilePath -> [String] -> m ProcessID
+spawnPID'Stack x xs = xfork $ catch (executeFile x True xs Nothing) $ \e ->
+			  if isDoesNotExistError e
+			    then do
+			      h <- getHomeDirectory
+			      let localBinPath = h </> ".local/bin"
+			          x'stack = localBinPath </> x
+			      b <- isExecutable x'stack
+			      if b 
+			        then executeFile "cls" True [] Nothing
+				else hPrint stderr (x ++ " can't be executed and not found in stack's local bin path: '" ++ localBinPath ++ "'\n" ++ show e)
+			    else throw e
+  where
+    isExecutable :: FilePath -> IO Bool
+    isExecutable f  = catch (getPermissions f >>= return . executable)
+                            (\e -> return (const False (e :: IOError)))
+
 
